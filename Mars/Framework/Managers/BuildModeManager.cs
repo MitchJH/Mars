f490bce2b69fcc1e87d09b528ecd31e53a6030b4
MitchJH <mheastie@hotmail.co.uk>
2016-05-02 20:58:47 +0100

    File reshuffle

---
+++ Mars/Framework/Managers/BuildModeManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mars
+{
+    public static class BuildModeManager
+    {
+        static BuildModeManager()
+        {
+        }
+    }
+}
